c3a6575d5e7c4ed02d08416125109c13b07ffb75
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-26 23:27:13 +0300

    root query and resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+let books = [
+  { name: "War and Peace", genre: "Russian Classic", id: "1" },
+  { name: "Crime and Punishment", genre: "Russian Classic", id: "2" },
+  { name: "Metamorphozis", genre: "Abstract", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -9,4 +17,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db / other soure
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
