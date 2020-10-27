24f558360cebaafbea7446783135b3cead53e930
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-27 17:37:43 +0300

    graphql root query, add mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require('./schema/schema')
-
+const mongoose = require('mongoose')
+const {MONGODB} = require('./mongodb-config')
 const app = express();
 
+// atlas mongodb connect
+mongoose.connect(MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.once('open', () => {
+  console.log('connection to database')
+})
 app.use("/graphql", graphqlHTTP({
-  schema
+  schema,
+  graphiql: true
 }));
 
 app.listen(5000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,64 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
 
 // dummy data
 let books = [
-  { name: "War and Peace", genre: "Russian Classic", id: "1" },
-  { name: "Crime and Punishment", genre: "Russian Classic", id: "2" },
-  { name: "Metamorphozis", genre: "Abstract", id: "3" },
+  { name: "War and Peace", genre: "Russian Classic", id: "1", authorId: "1" },
+  {
+    name: "Crime and Punishment",
+    genre: "Russian Classic",
+    id: "2",
+    authorId: "2",
+  },
+  { name: "Metamorphozis", genre: "Abstract", id: "3", authorId: "3" },
+  { name: "Gambler", genre: "Russian Classic", id: "4", authorId: "2" },
+  { name: "Anna Karenina", genre: "Russian Classic", id: "5", authorId: "1" },
+  { name: "Castle", genre: "Abstract", id: "6", authorId: "3" },
+];
+
+let authors = [
+  { name: "Leo Tolstoy", age: 44, id: "1" },
+  { name: "Fedyor Dostoevskiy", age: 41, id: "2" },
+  { name: "Franz Kafka", age: 38, id: "3" },
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
@@ -24,12 +67,32 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other soure
+        console.log(typeof args.id);
         return _.find(books, { id: args.id });
       },
     },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      },
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent,args) {
+        return books
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors
+      }
+    }
   },
 });
 
